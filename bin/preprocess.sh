6cda858a2b0a7fb3ad1f759444156440aaad21f5
Rian Wouters <Rian.Wouters@philips.com>
2014-04-08 14:45:56 +0200

    Provide sensible usage output on error

--- bin/preprocess.sh
+++ bin/preprocess.sh
@@ -3,5 +3,7 @@
 cd $OSSAPS_FOLDER
 bundle install
 bundle exec rake aps:process INPUT_DIR=$1 OUTPUT_DIR=$2
+EXIT_CODE=$?
+[ $EXIT_CODE -ne 0 ] && { echo "Usage: $0 INPUT_DIR=<input_dir> OUTPUT_DIR=<output_dir>"; }
 
-exit $?
+exit $EXIT_CODE
