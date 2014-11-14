f8645b71914a13b0739690d04dc31a32f3538e8e
Rian Wouters <Rian.Wouters@philips.com>
2014-11-14 02:48:15 +0100

    Export designated room column

--- lib/csv_exporter.rb
+++ lib/csv_exporter.rb
@@ -1,7 +1,7 @@
 class CsvExporter
 
-  def self.to_row luminaire
-    [luminaire.ctn, luminaire.fam_name, luminaire.itemnr, luminaire.has_colorsheet?.to_s, luminaire.has_view3d?.to_s, luminaire.is_complete?.to_s]
+  def self.to_row lum
+    [lum.ctn, lum.fam_name, lum.itemnr, lum.has_colorsheet?.to_s, lum.has_view3d?.to_s, lum.has_designated_room?.to_s, lum.is_complete?.to_s]
   end
 
   def self.export_all luminaires, output_path
@@ -15,6 +15,6 @@ class CsvExporter
     end
   end
 
-  HEADER = ["CTN", "Fam name", "Item nbr", "Has Colorsheet", "Has 3DView", "Complete?"]
+  HEADER = ["CTN", "Fam name", "Item nbr", "Has Colorsheet", "Has 3DView", "Has designated room", "Complete?"]
 
 end
--- spec/csv_exporter_spec.rb
+++ spec/csv_exporter_spec.rb
@@ -12,7 +12,7 @@ describe CsvExporter do
   let(:lum2) { FactoryGirl.build(:luminaire) }
   let(:luminaires) { [lum1, lum2] }
 
-  HEADER = ["CTN", "Fam name", "Item nbr", "Has Colorsheet", "Has 3DView", "Complete?"]
+  HEADER = ["CTN", "Fam name", "Item nbr", "Has Colorsheet", "Has 3DView", "Has designated room", "Complete?"]
 
   describe '::HEADER' do
     subject{ described_class::HEADER }
@@ -20,13 +20,14 @@ describe CsvExporter do
   end
 
   describe '.to_row' do
-    luminaire = FactoryGirl.build(:luminaire)
-    let(:expected_values){ [luminaire.ctn, luminaire.ctn[0..4], luminaire.itemnr, luminaire.has_colorsheet?.to_s, luminaire.has_view3d?.to_s, luminaire.is_complete?.to_s] }
-    subject { described_class.to_row luminaire }
+    lum = FactoryGirl.build(:luminaire)
+    let(:expected_values){ [lum.ctn, lum.ctn[0..4], lum.itemnr, lum.has_colorsheet?.to_s, lum.has_view3d?.to_s, lum.has_designated_room?.to_s, lum.is_complete?.to_s] }
+    subject { described_class.to_row lum }
     before do
-      luminaire.stub(:has_colorsheet?).and_return("colorsheet")
-      luminaire.stub(:has_view3d?).and_return("view3d")
-      luminaire.stub(:is_complete?).and_return("complete")
+      lum.stub(:has_colorsheet?).and_return("colorsheet")
+      lum.stub(:has_view3d?).and_return("view3d")
+      lum.stub(:has_designated_room?).and_return("room")
+      lum.stub(:is_complete?).and_return("complete")
     end
     its(:size){ should eql described_class::HEADER.size}
     it{ should eql expected_values}
